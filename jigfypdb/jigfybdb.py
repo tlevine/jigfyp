4ccd6d5a0b573f6c0e1e0b9d21dd99176b5e8ef2
Thomas Levine <_@thomaslevine.com>
2015-08-12 12:41:40 -0400

    write all docstrings

--- jigfypdb/jigfybdb.py
+++ jigfypdb/jigfybdb.py
@@ -1,6 +1,6 @@
 def put(db, key, value):
     '''
-    Save a value to the level database.
+    Save a record to the level database.
 
     :param leveldb.LevelDB db: The levelDB instance to query...'
     :type key: tuple of bytes
@@ -9,13 +9,46 @@ def put(db, key, value):
     '''
 
 def read_one(db, key):
-    pass
+    '''
+    Read one record from the level database.
 
-def read_many(db, keys):
-    pass
+    :param leveldb.LevelDB db: The levelDB instance to query...'
+    :type key: tuple of bytes
+    :param key: The key
+    :rtype: tuple containing two bytes elements
+    :returns: The raw key and value corresponding to that key
+    '''
+
+def read_many(db, key_prefix):
+    '''
+    Read several records from the level database. All records whose keys
+    begin with the particular prefix are returned.
+
+    :param leveldb.LevelDB db: The levelDB instance to query...'
+    :type key_prefix: tuple of bytes
+    :param key_prefix: A left-aligned subset of the key values.
+    :rtype: iterable of tuples, each containing two bytes elements
+    :returns: Iterable of keys-value pairs
+    '''
 
 def delete_one(db, key):
-    pass
+    '''
+    Delete one record from the level database.
 
-def delete_many(db, keys):
-    pass
+    :param leveldb.LevelDB db: The levelDB instance to query...'
+    :type key: tuple of bytes
+    :param key: The key
+    :rtype: None
+    :returns: Nothing
+    '''
+
+def delete_many(db, key_prefix):
+    '''
+    Delete several records from the level database.
+
+    :param leveldb.LevelDB db: The levelDB instance to query...'
+    :type key_prefix: tuple of bytes
+    :param key_prefix: A left-aligned subset of the key values.
+    :rtype: None
+    :returns: Nothing
+    '''
