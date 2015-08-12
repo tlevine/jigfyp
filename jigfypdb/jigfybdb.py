3d9f60626d3b1afec88b7b8aa4b82182d33a193a
Thomas Levine <_@thomaslevine.com>
2015-08-12 12:43:55 -0400

    make it a class so we can parametrize delimeter and stuff

--- jigfypdb/jigfybdb.py
+++ jigfypdb/jigfybdb.py
@@ -1,54 +1,62 @@
-def put(db, key, value):
-    '''
-    Save a record to the level database.
+class Jigfypdb:
+    def __init__(self, db, delimiter = b'!', highest_character = b'~'):
+        self.db = db
+        for name in ['delimiter', 'highest_character']:
+            if not isinstance(name, bytes):
+                raise TypeError('%s must be bytes.' % name)
+            setattr(self, name, locals()[name])
 
-    :param leveldb.LevelDB db: The levelDB instance to query...'
-    :type key: tuple of bytes
-    :param key: The key
-    :param bytes value: The value
-    '''
+    def put(db, key, value):
+        '''
+        Save a record to the level database.
 
-def read_one(db, key):
-    '''
-    Read one record from the level database.
+        :param leveldb.LevelDB db: The levelDB instance to query...'
+        :type key: tuple of bytes
+        :param key: The key
+        :param bytes value: The value
+        '''
 
-    :param leveldb.LevelDB db: The levelDB instance to query...'
-    :type key: tuple of bytes
-    :param key: The key
-    :rtype: tuple containing two bytes elements
-    :returns: The raw key and value corresponding to that key
-    '''
+    def read_one(db, key):
+        '''
+        Read one record from the level database.
 
-def read_many(db, key_prefix):
-    '''
-    Read several records from the level database. All records whose keys
-    begin with the particular prefix are returned.
+        :param leveldb.LevelDB db: The levelDB instance to query...'
+        :type key: tuple of bytes
+        :param key: The key
+        :rtype: tuple containing two bytes elements
+        :returns: The raw key and value corresponding to that key
+        '''
 
-    :param leveldb.LevelDB db: The levelDB instance to query...'
-    :type key_prefix: tuple of bytes
-    :param key_prefix: A left-aligned subset of the key values.
-    :rtype: iterable of tuples, each containing two bytes elements
-    :returns: Iterable of keys-value pairs
-    '''
+    def read_many(db, key_prefix):
+        '''
+        Read several records from the level database. All records whose keys
+        begin with the particular prefix are returned.
 
-def delete_one(db, key):
-    '''
-    Delete one record from the level database.
+        :param leveldb.LevelDB db: The levelDB instance to query...'
+        :type key_prefix: tuple of bytes
+        :param key_prefix: A left-aligned subset of the key values.
+        :rtype: iterable of tuples, each containing two bytes elements
+        :returns: Iterable of keys-value pairs
+        '''
 
-    :param leveldb.LevelDB db: The levelDB instance to query...'
-    :type key: tuple of bytes
-    :param key: The key
-    :rtype: None
-    :returns: Nothing
-    '''
+    def delete_one(db, key):
+        '''
+        Delete one record from the level database.
 
-def delete_many(db, key_prefix):
-    '''
-    Delete several records from the level database.
+        :param leveldb.LevelDB db: The levelDB instance to query...'
+        :type key: tuple of bytes
+        :param key: The key
+        :rtype: None
+        :returns: Nothing
+        '''
 
-    :param leveldb.LevelDB db: The levelDB instance to query...'
-    :type key_prefix: tuple of bytes
-    :param key_prefix: A left-aligned subset of the key values.
-    :rtype: None
-    :returns: Nothing
-    '''
+    def delete_many(db, key_prefix):
+        '''
+        Delete several records from the level database.
+
+        :param leveldb.LevelDB db: The levelDB instance to query...'
+        :type key_prefix: tuple of bytes
+        :param key_prefix: A left-aligned subset of the key values.
+        :rtype: None
+        :returns: Nothing
+        '''
