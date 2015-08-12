5c209a90705f2cf0d4e0ded6cbbd3707e404d243
Thomas Levine <_@thomaslevine.com>
2015-08-12 12:33:36 -0400

    think about the high-level api

---
+++ jigfypdb/jigfybdb.py
@@ -0,0 +1,21 @@
+class JigFypDB:
+    def __init__(self, db):
+        '''
+        :param leveldb.LevelDB db: The levelDB instance to query...'
+        '''
+
+
+def put(db, key, value):
+    pass
+
+def read_one(db, key):
+    pass
+
+def read_many(db, keys):
+    pass
+
+def delete_one(db, key):
+    pass
+
+def delete_many(db, keys):
+    pass
